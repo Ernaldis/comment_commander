12c569de6e6e314986e583c8f94cc5f2cb362c68
Timothy Chandler <ernaldis@protonmail.com>
2023-04-12 17:14:48 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,92 @@
+#![allow(unused)]
+
+extern crate termion;
+
+use clap::Parser;
+use std::env;
+use std::io::{stdin, stdout, Write};
+use std::iter::TakeWhile;
+use std::process::Command;
+use std::thread::sleep;
+use std::time::Duration;
+use termion::cursor::DetectCursorPos;
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use termion::{clear, cursor};
+
+#[derive(Parser)]
+struct Cli {
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let lines: Vec<String> = content
+        .lines()
+        .take_while(|line: &&str| line.starts_with("#"))
+        .map(|line| line[2..].trim().to_string())
+        .collect();
+
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    let mut position = 0;
+
+    write!(
+        stdout,
+        "{}{}{}{}",
+        termion::cursor::Save,
+        clear::CurrentLine,
+        lines[position],
+        termion::cursor::Restore
+    )
+    .unwrap();
+    stdout.flush().unwrap();
+
+    for c in stdin().keys() {
+        match c.unwrap() {
+            Key::Char('q') => {
+                write!(stdout, "{}{}", clear::CurrentLine, "\n\r").unwrap();
+                stdout.flush().unwrap();
+                break;
+            }
+            Key::Up => {
+                if position > 0 {
+                    position -= 1;
+                } else {
+                    position = lines.len() - 1;
+                }
+            }
+            Key::Down => {
+                if position < lines.len() - 1 {
+                    position += 1;
+                } else {
+                    position = 0;
+                }
+            }
+            Key::Char('\n') => {
+                let args: Vec<&str> = lines[position].split_whitespace().collect();
+                let output = Command::new(&args[0])
+                    .args(&args[1..])
+                    .output()
+                    .expect("Command failed");
+                let output_str = String::from_utf8_lossy(&output.stdout).replace('\n', "\n\r");
+                write!(stdout, "{}{}", clear::CurrentLine, output_str).unwrap();
+                stdout.flush().unwrap();
+                break;
+            }
+            _ => {}
+        }
+        // Move the cursor down one line and write some text
+        write!(
+            stdout,
+            "{}{}{}",
+            clear::CurrentLine,
+            lines[position],
+            termion::cursor::Restore
+        )
+        .unwrap();
+        stdout.flush().unwrap();
+    }
+}
